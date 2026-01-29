aafa60b9cf8aefd20dff728b7dbabd6a14e61873
Patchara Phookheaw <patcharaph@gmail.com>
2026-01-29 00:48:58 -0500

    Task 5: configure Redux store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+
+// Create a Redux store using configureStore from Redux Toolkit
+const store = configureStore({
+  reducer: {
+    // 'cart' คือชื่อ slice ใน global state
+    cart: cartReducer,
+  },
 });
-export default store
+
+export default store;
