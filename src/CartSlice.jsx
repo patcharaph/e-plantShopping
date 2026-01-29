3eff049b77d1227bef797cafd5467b2f3504f35e
Patchara Phookheaw <patcharaph@gmail.com>
2026-01-29 00:21:29 -0500

    Implement cart reducers: add, remove, update quantity

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [], // cart items array
   },
   reducers: {
     addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+      const { name, image, cost } = action.payload;
 
-    
+      const existingItem = state.items.find((item) => item.name === name);
+
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
+    },
+
+    removeItem: (state, action) => {
+      // action.payload = name
+      state.items = state.items.filter((item) => item.name !== action.payload);
+    },
+
+    updateQuantity: (state, action) => {
+      // action.payload = { name, quantity }
+      const { name, quantity } = action.payload;
+
+      const itemToUpdate = state.items.find((item) => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
 });
